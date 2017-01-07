09ef440b25c5669f8f99e25dd9b114383a32bfa2
Nathan McMinn <nathan.mcminn@alfresco.com>
2017-01-07 08:32:47 -0600

    Removed unused data models

--- server/app/models/aqdata.js
+++
@@ -1,6 +0,0 @@
-/*
-Model for air quality data
-thdata = temp and humidity
-dsdata = dust sensor data
-gsdata = gas sensor data
-*/
--- server/app/models/initdata.js
+++
@@ -1,6 +0,0 @@
-/*
-Model for air quality data
-time = time this unit id initialized
-id = id of the AQ sensor pack
-dataversion = data format version sent by sensor pack
-*/
